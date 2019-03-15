ef00e67125c244b2ef72b96a87f79e3eb417f1fe
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2019-03-15 04:31:44 +0100

    Add main.py doc

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from source.cli import print_menu
 import numpy as np
 
+# Necessary for starting Numpy generated random numbers in a well-defined initial state
+# in order to obtain reproducible results
 np.random.seed(15)
 
+# Print main menu
 print_menu()
