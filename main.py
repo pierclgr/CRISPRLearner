f3aaa37831317fb979e10c568959ff397216e289
Pierpasquale Colagrande <pierpasquale.colagrande@gmail.com>
2019-04-07 15:59:45 +0200

    Add new trained models

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 # Import libraries and modules
 from source.cli import print_menu
-import numpy as np
-
-# Necessary for starting Numpy generated random numbers in a well-defined initial state
-# in order to obtain reproducible results
-np.random.seed(15)
 
 # Print main menu
 print_menu()
